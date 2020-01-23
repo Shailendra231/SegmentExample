2a3b53dc4d33946515511263e47dcd651e443636
Shailendra <ramkumarshailendra@gmail.com>
2020-01-23 15:35:13 +0530

    Initial Commit

---
+++ SegmentExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentExample
+//
+//  Created by nitin chawla on 23/01/20.
+//  Copyright © 2020 nitin chawla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
